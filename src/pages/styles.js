f76b5c8fa224f126262a22f643e02cf5505a6f85
Camila Moura <camilapaleo@gmail.com>
2020-07-16 16:14:58 +0200

    added img and background in heroBanner

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -1,13 +1,27 @@
 import styled from "styled-components";
 
+// import background from "@site/static/img/background.svg";
+
 export const HeroBanner = styled.div`
-  padding: 0 2rem;
   height: 100vh;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  position: relative;
+  width: 100vw;
   overflow: hidden;
+  display: flex;
+
+  background: url("/img/background.svg") no-repeat right bottom;
+  background-size: 700px;
+`;
+
+export const Content = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  width: 100%;
+  justify-content: space-around;
+`;
+
+export const ContentText = styled.div`
+  flex-direction: column;
+  align-self: center;
 `;
 
 export const HeroTitle = styled.h1`
@@ -28,9 +42,10 @@ export const HeroButtons = styled.div`
   justify-content: baseline;
 `;
 
-export const ContentText = styled.div``;
-
-export const ContentImg = styled.div``;
+export const ContentImg = styled.div`
+  align-self: flex-end;
+  width: 600px;
+`;
 
 export const ContentCard = styled.div`
   display: flex;
