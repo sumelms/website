604df0912dea99731a255949110ed9ae29276ea2
Camila Moura <camilapaleo@gmail.com>
2020-07-18 23:15:09 +0200

    fixed font size

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 6rem;
+  padding: 4rem;
 `;
 
 export const Title = styled.h2`
@@ -10,5 +10,5 @@ export const Title = styled.h2`
 `;
 
 export const Content = styled.div`
-  padding: 2rem 0;
+  padding: 2rem 0 2rem;
 `;
